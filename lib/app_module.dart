1859783dc42f4af5df8f3fde6a36f42058062538
plucenio <plucenio@gmail.com>
2021-03-15 10:43:56 -0300

    Applied flutter_modular: ^3.0.1

---
+++ lib/app_module.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'home_page.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [];
+
+  @override
+  final List<ModularRoute> routes = [
+    ChildRoute(
+      Modular.initialRoute,
+      child: (_, __) => HomePage(),
+    ),
+  ];
+}
